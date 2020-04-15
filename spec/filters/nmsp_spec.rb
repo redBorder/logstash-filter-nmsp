f6cf3c7d878581ca50f4e901cb7f2f9d35fc5577
laeticia-ds <ldossantos@redborder.com>
2020-04-15 08:57:18 +0100

    Adding new testing file in NMSP plugin

---
+++ spec/filters/nmsp_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require 'spec_helper'
+require "logstash/filters/nmsp"
+require 'dalli'
+
+describe LogStash::Filters::Nmsp do
+  describe "Set to Hello World" do
+    let(:config) do <<-CONFIG
+      filter {
+        nmsp {
+          message => "Hello World"
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => "some text") do
+      expect(subject.get("message")).to eq('Hello World')
+    end
+  end
+end
